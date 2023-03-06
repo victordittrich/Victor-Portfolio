6d5f0edc0261ffde211d0e0a4c270760671fd110
Victor Hugo <vituhds0610@gmail.com>
2023-03-06 16:13:01 -0300

    services done

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,29 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
+const modalViews = document.querySelectorAll('.services__modal')
+const modalBtns = document.querySelectorAll('.services__button')
+const modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb,i)=>{
+    mb.addEventListener('click', () =>{
+        modal(i)
+    }
+)})
+
+modalClose.forEach((mc) =>{
+    mc.addEventListener('click', () =>{
+        modalViews.forEach((mv) =>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+
+
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 

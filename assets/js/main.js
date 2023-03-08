9b3dbcb72bd5239b566eca18e1c8dd4aa132d560
Victor Hugo <vituhds0610@gmail.com>
2023-03-08 17:20:54 -0300

    carousel test

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,30 @@ modalClose.forEach((mc) =>{
     })
 })
 
+/*=============== SERVICES MODAL ===============*/
+
+// const modalViews = document.querySelectorAll('.services__modal')
+// const modalBtns = document.querySelectorAll('.services__button')
+// const modalClose = document.querySelectorAll('.services__modal-close')
+
+// let modal = function(modalClick){
+//     modalViews[modalClick].classList.add('active-modal')
+// }
+
+// modalBtns.forEach((mb,i)=>{
+//     mb.addEventListener('click', () =>{
+//         modal(i)
+//     }
+// )})
+
+// modalClose.forEach((mc) =>{
+//     mc.addEventListener('click', () =>{
+//         modalViews.forEach((mv) =>{
+//             mv.classList.remove('active-modal')
+//         })
+//     })
+// })
+
 
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
@@ -59,25 +83,25 @@ linkWork.forEach(l=> l.addEventListener('click', activeWork))
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
     
-const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
+// const scrollActive = () =>{
+//   	const scrollY = window.pageYOffset
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+// 	sections.forEach(current =>{
+// 		const sectionHeight = current.offsetHeight,
+// 			  sectionTop = current.offsetTop - 58,
+// 			  sectionId = current.getAttribute('id'),
+// 			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
-}
-window.addEventListener('scroll', scrollActive)
+// 		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+// 			sectionsClass.classList.add('active-link')
+// 		}else{
+// 			sectionsClass.classList.remove('active-link')
+// 		}                                                    
+// 	})
+// }
+// window.addEventListener('scroll', scrollActive)
 
 
 /*=============== LIGHT DARK THEME ===============*/ 
